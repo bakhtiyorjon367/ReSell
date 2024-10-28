e4022021d4041a2c9091db7fd8af0ae985de5272
bakhtiyorjon367 <bakhtee.0302@gmail.com>
2024-10-28 22:33:21 +0900

    feat: components related Modules

--- apps/resell-api/src/components/components.module.ts
+++ apps/resell-api/src/components/components.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { MemberModule } from './member/member.module';
 import { ProductModule } from './product/product.module';
+import { AuthModule } from './auth/auth.module';
+import { CommentModule } from './comment/comment.module';
+import { LikeModule } from './like/like.module';
+import { ViewModule } from './view/view.module';
+import { FollowModule } from './follow/follow.module';
+import { BoardArticleModule } from './board-article/board-article.module';
 
 @Module({
-  imports: [MemberModule, ProductModule]
+  imports: [
+    MemberModule, 
+    AuthModule, 
+    ProductModule, 
+    CommentModule, 
+    LikeModule, 
+    ViewModule, 
+    FollowModule, 
+    BoardArticleModule]
 })
 export class ComponentsModule {}
