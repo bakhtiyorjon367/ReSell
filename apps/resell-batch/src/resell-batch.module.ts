ebf22d9d12d20df277af863f851f12c7326f6d1d
bakhtiyorjon367 <bakhtee.0302@gmail.com>
2024-11-05 22:09:38 +0900

    fix: modify batch module and connect database

--- apps/resell-batch/src/resell-batch.module.ts
+++ apps/resell-batch/src/resell-batch.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ResellBatchController } from './resell-batch.controller';
 import { ResellBatchService } from './resell-batch.service';
 import {ConfigModule} from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), DatabaseModule, ScheduleModule.forRoot()],
   controllers: [ResellBatchController],
   providers: [ResellBatchService],
 })
